69d90df4bc780deb95bbff57378444d16e5e1741
venicephua <venicepsy@gmail.com>
2025-04-21 18:33:42 +0800

    fix docker-compose issues

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -6,7 +6,7 @@ dotenv.config();
 // Create a connection pool
 const pool = new Pool({
   user: process.env.DB_USER || 'venice',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'postgres',
   database: process.env.DB_NAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   port: parseInt(process.env.DB_PORT || '5432'),
